def3e5ddbc5b3a5c34c06e5041db88c63cb0bd8e
Gabriel Franklin <franklingabriel158@gmail.com>
2021-07-05 22:00:19 -0300

    Insere tagH1 addDiv centerDiv script.js

---
+++ exercises/5_2/script.js
@@ -0,0 +1,12 @@
+let tagH1 = document.createElement('h1');
+tagH1.innerText = 'Exercício 5.2 - Javascript DOM';
+document.body.appendChild(tagH1);
+
+let addDiv = document.createElement('div');
+addDiv.classList.add('.main-content');
+document.body.appendChild(addDiv);
+
+
+let centerDiv = document.createElement('div');
+centerDiv.classList.add('center-content');
+document.body.addDiv.appendChild(centerDiv);
