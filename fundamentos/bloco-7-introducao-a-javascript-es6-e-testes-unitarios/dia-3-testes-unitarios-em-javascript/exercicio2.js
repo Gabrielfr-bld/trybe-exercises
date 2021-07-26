e6abc15548e1eec02d846f407f7f1f63c76accc2
Gabriel Franklin <franklingabriel158@gmail.com>
2021-07-26 19:15:07 -0300

    Correcao nos exercicios 1 e 2, e adiciona exercicio 3

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/exercicio2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/exercicio2.js
@@ -9,5 +9,8 @@ function myRemove(arr, item) {
   }
   return newArr;
 }
-console.log(myRemove([1,2,3,4], 3));
-console.log(myRemove([1,2,3,4], 5));
+
+assert.strictEqual(typeof myRemove, 'function');
+assert.deepStrictEqual(myRemove([1,2,3,4],3), [1,2,4]);
+assert.notDeepStrictEqual(myRemove([1,2,3,4], 3), [1,2,3,4]);
+assert.deepStrictEqual(myRemove([1,2,3,4], 5), [1,2,3,4]);
