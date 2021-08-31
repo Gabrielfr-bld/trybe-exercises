177df0fa2fe2504410eb211fab6ba2ce3b42f9e1
Gabriel Franklin <franklingabriel158@gmail.com>
2021-08-31 20:22:26 -0300

    Finalizado exercicio criar pokedex

--- front-end/bloco-11-introducao-a-react/dia-2-componentes-react/my-pokedex/src/components/Pokemon.js
+++ front-end/bloco-11-introducao-a-react/dia-2-componentes-react/my-pokedex/src/components/Pokemon.js
@@ -1,10 +1,20 @@
 import React from 'react';
-
+// import PropType from 'prop-types';
 
 class Pokemon extends React.Component {
     render() {
+        const { pokemon } = this.props;
+
         return(
-           <span>Pokemon</span>     
+           <section className="pokemon">
+               <p>{pokemon.name}</p>
+               <p>{pokemon.type}</p>
+               <p>{`Average weight: ${pokemon.averageWeight.value} ${pokemon.averageWeight.measurementUnit}`}</p>
+               <span>
+                <img src={ pokemon.image } alt={`${pokemon.name} sprite`}/>
+              </span>  
+           </section>  
+
         );
     }
 }
