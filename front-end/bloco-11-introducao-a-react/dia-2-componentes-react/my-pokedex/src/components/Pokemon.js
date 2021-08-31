1bdb3e4ad5de77bd84590a522991f9ee0f26fe4c
Gabriel Franklin <franklingabriel158@gmail.com>
2021-08-31 19:19:09 -0300

    Iniciando exercicios do segundo dia de React

---
+++ front-end/bloco-11-introducao-a-react/dia-2-componentes-react/my-pokedex/src/components/Pokemon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class Pokemon extends React.Component {
+    render() {
+        return(
+           <span>Pokemon</span>     
+        );
+    }
+}
+
+export default Pokemon;
